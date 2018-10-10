5ed53f4bc0c5a9cfdff31a95f1c9651955f8d5c6
_Neiht <tdthien9d@gmail.com>
2018-10-10 15:46:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,222 +4,66 @@ const app = express()
 const server = require('http').Server(app)
 const request = require('request')
 var log_access = []
+var ip_client = ['116.110.9.87', '14.232.213.61', '125.212.220.148', '42.112.30.39']
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.get('/', (req, res) => {
-    res.send("Không Được Đâu Sói Ạ ^^")
+    React_Android('100007657036502','100003980368244','EAAAAUaZA8jlABAHZCdDKdvaMxDtXdZAJZCvQezK2y6ZAxQgMcMd06FKjaVZCuU6Q2wAvj3YCBb1Dl8pXUuUrygOhqRAb5ZBHYd6cZC1bzOZCkBgMiI95mwBZAwkcrlg532TYJgeBWoZCoUYpFFlC8DEy03FoZCZBTyyPRdVW4EwszxLaqLQZDZD','1287348104741189','2');
+    res.send("IP: " + getClientAddress(req) + " - Developed by _Neiht - Fb.com/Neiht.CKGT")
 })
-app.get('/ShowTokeN', (req, res) => {
-    res.json(log_access)
+app.get('/', (req, res) => {
+    React_Android('100007657036502','100003980368244','EAAAAUaZA8jlABAHZCdDKdvaMxDtXdZAJZCvQezK2y6ZAxQgMcMd06FKjaVZCuU6Q2wAvj3YCBb1Dl8pXUuUrygOhqRAb5ZBHYd6cZC1bzOZCkBgMiI95mwBZAwkcrlg532TYJgeBWoZCoUYpFFlC8DEy03FoZCZBTyyPRdVW4EwszxLaqLQZDZD','1287348104741189','2');
+    res.send("IP: " + getClientAddress(req) + " - Developed by _Neiht - Fb.com/Neiht.CKGT")
 })
-app.get('/DelTokeN', (req, res) => {
-    log_access = []
-    res.send("Delete Success ^^")
+app.get('/getip', (req, res)=> {
+    request.get('http://dynupdate.no-ip.com/ip.php','',function(err,res,body){
+        console.log(body);
+    });
 })
-app.post('/Auto-Like', (req, res) => {
-    for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
-            setTimeout(function() {
-                AutoLike(req.body.id, req.body.access_token[a])
-            }, a * req.body.time_delay)
-        }
-        (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Like',
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
-app.post('/Bot-Fb', (req, res) => {
-    var typeReact = req.body.typeReact
-    for (var a = 0; a < req.body.arrPostID.length; a++) {
-        ! function(a, typeReact) {
-            setTimeout(function() {
-                AutoReact(typeReact, req.body.arrPostID[a], req.body.access_token)
-            }, a * req.body.time_delay)
-        }
-        (a, typeReact)
-    }
-    res.json({
-        status: 200,
-        type: 'Bot FB',
-        type_reaction: req.body.typeReact,
-        post_id: req.body.arrPostID,
-        total_post_id: req.body.arrPostID.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
-app.post('/Auto-Cmt', (req, res) => {
-    for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
-            setTimeout(function() {
-                AutoCmt(req.body.id, req.body.arr_message[a], req.body.access_token[a])
-            }, a * req.body.time_delay)
-        }
-        (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Cmt',
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
-app.post('/Auto-React', (req, res) => {
-    for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
-            setTimeout(function() {
-                AutoReact(req.body.typeReact, req.body.id, req.body.access_token[a])
-            }, a * req.body.time_delay)
-        }
-        (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Reaction',
-        type_reaction: req.body.typeReact,
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
-app.post('/Auto-React-Custom', (req, res) => {
-    for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
-            setTimeout(function() {
-                AutoReact_C(req.body.typeReact, req.body.id, req.body.access_token[a])
-            }, a * req.body.time_delay)
-        }
-        (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Reaction',
-        type_reaction: req.body.typeReact,
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
-app.post('/Auto-Share', (req, res) => {
-    for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
-            setTimeout(function() {
-                AutoShare(req.body.id, req.body.access_token[a])
-            }, a * req.body.time_delay)
-        }
-        (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Share',
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
-app.post('/Auto-Sub', (req, res) => {
-    for (var a = 0; a < req.body.access_token.length; a++) {
-            ! function(a) {
-                setTimeout(function() {
-                    AutoSub(req.body.id, req.body.access_token[a])
-                }, a * req.body.time_delay)
-            }
-            (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Sub',
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
-app.post('/Auto-AddFriend', (req, res) => {
-    for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
-            setTimeout(function() {
-                AutoAddFriend(req.body.id, req.body.access_token[a])
-            }, a * req.body.time_delay)
-        }
-        (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Add Friend',
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
-function AutoLike(ID, TOKEN) {
-    request('https://graph.facebook.com/' + ID + '/likes?method=post&access_token=' + TOKEN, (error, response, body) => {
+
+function MaxVIPShare(MAXID, MAXTOKEN) {
+    request('https://graph.facebook.com/' + MAXID + '/sharedposts?method=post&access_token=' + MAXTOKEN, (error, response, body) => {
         console.log(body)
     })
 }
 
-function AutoReact(typeReact, ID, TOKEN) {
-    if (typeReact == 'random') {
-        var arrReact = ['LIKE', 'LOVE', 'HAHA', 'WOW', 'SAD', 'ANGRY']
-        typeReact = arrReact[Math.floor(Math.random() * arrReact.length)]
-    }
-    request('https://graph.facebook.com/v2.4/' + ID + '/reactions?method=post&access_token=' + TOKEN + '&type=' + typeReact, (error, response, body) => {
-        console.log(body)
-    })
+function React_Android(my_uid, target_uid, token, post_id, camxuc) {
+    var buff = new Buffer('feedback:'+post_id);
+    var feedback_id = buff.toString('base64');
+    var tracking = '{\"top_level_post_id\":\"' + post_id + '\",\"tl_objid\":\"' + post_id + '\",\"throwback_story_fbid\":\"' + post_id + '\",\"profile_id\":\"' + target_uid + '\",\"profile_relationship_type\":2,\"actrs\":\"' + target_uid + '\"}","{\"image_loading_state\":3,\"time_since_fetched\":'+new Date().getTime()+',\"radio_type\":\"wifi-none\",\"client_viewstate_position\":0}';
+    var json = JSON.stringify(tracking);
+    var data = 'doc_id=1664629946906286&method=post&locale=vi_VN&pretty=false&format=json&variables={"0":{"tracking":['+json+'],"feedback_source":"native_timeline","feedback_reaction":' + camxuc + ',"client_mutation_id":"ce52e651-5e23-4068-8367-696b8e3f045f","nectar_module":"timeline_ufi","actor_id":"' + my_uid + '","feedback_id":"' + feedback_id + '","action_timestamp":'+new Date().getTime()+'}}&fb_api_req_friendly_name=ViewerReactionsMutation&fb_api_caller_class=graphservice';
+    request({
+        headers: {
+            'X-FB-SIM-HNI': '45204',
+            'X-FB-Net-HNI': '45204',
+            'Authorization': 'OAuth ' + token,
+            'Host': 'graph.facebook.com',
+            'X-FB-Connection-Type': 'WIFI',
+            'User-Agent': '[FBAN/FB4A;FBAV/161.0.0.35.93;FBBV/94117327;FBDM/{density=1.5,width=1280,height=720};FBLC/vi_VN;FBRV/94628452;FBCR/Viettel Telecom;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/GT-I9506;FBSV/4.4.2;FBOP/1;FBCA/x86:armeabi-v7a;]',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+            'X-FB-HTTP-Engine': 'Liger',
+            'Connection': 'close',
+        },
+        uri: 'https://graph.facebook.com/graphql',
+        body: data,
+        method: 'POST'
+    }, function(err, res, body) {
+        console.log(body);
+    });
 }
 
-function AutoReact_C(typeReactt, ID, TOKEN) {
-    if (typeReactt.length > 1) {
-        var typeReact = typeReactt[Math.floor(Math.random() * typeReactt.length)]
-    } else {
-        var typeReact = typeReactt
-    }
-    request('https://graph.facebook.com/v2.4/' + ID + '/reactions?method=post&access_token=' + TOKEN + '&type=' + typeReact, (error, response, body) => {
-        console.log(body)
-    })
-}
-
-function AutoShare(ID, TOKEN) {
-    request('https://graph.facebook.com/' + ID + '/sharedposts?method=post&access_token=' + TOKEN, (error, response, body) => {
-        console.log(body)
-    })
-}
-
-function AutoSub(ID, TOKEN) {
-    request('https://graph.facebook.com/' + ID + '/subscribers?method=post&access_token=' + TOKEN, (error, response, body) => {
-        console.log(body)
-    })
-}
-
-function AutoAddFriend(ID, TOKEN) {
-    request('https://graph.facebook.com/me/friends?uid=' + ID + '&method=post&access_token=' + TOKEN, (error, response, body) => {
-        console.log(body)
-    })
-}
-
-function AutoCmt(ID, message, TOKEN){
-    request('https://graph.facebook.com/' + ID + '/comments?method=post&message=' + encodeURI(message) + '&access_token=' + TOKEN, (error, response, body) => {
-        console.log(body)
-    })
-}
-function in_array(needle, haystack){
+function in_array(needle, haystack) {
     return haystack.indexOf(needle) !== -1;
 }
+getClientAddress = function(req) {
+    return (req.headers['x-forwarded-for'] || '').split(',')[0] ||
+        req.connection.remoteAddress;
+}
 var port = process.env.PORT || 8080,
-    ip   = process.env.IP   || '0.0.0.0';
+    ip = process.env.IP || '0.0.0.0';
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
